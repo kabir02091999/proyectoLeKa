132cf680dbb6d589b386e2794ae3b501fc4bee06
kabir02091999 <kabir.02091999@gmail.com>
2021-04-12 16:57:05 -0400

    cambios

--- src/componetes/Inicio.jsx
+++ src/componetes/Inicio.jsx
@@ -6,13 +6,25 @@ import LaptopWindowsIcon from '@material-ui/icons/LaptopWindows';
 import PhoneIphoneIcon from '@material-ui/icons/PhoneIphone';
 import SportsBasketballIcon from '@material-ui/icons/SportsBasketball';
 import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import AppleIcon from '@material-ui/icons/Apple';
+import AndroidIcon from '@material-ui/icons/Android';
+
+//iconos de pie pagina
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TelegramIcon from '@material-ui/icons/Telegram';
+
+//imagenes
+import Captura from'../imagenes/Captura.jpg'
 
 const Inicio = () => {
     return ( <div className="main">
 
-        <div className="imagensEventos">
+        <div className="imagenesEventos">
 
-            <h1>imagenes</h1>
+            <img src={Captura} alt="imagen" className="imagen" />
 
         </div>
         <div className="productos" >
