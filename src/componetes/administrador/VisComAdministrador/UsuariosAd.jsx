40567ba7dc4a605ac208fbfcff4532bab0dd3ba9
kabir02091999 <kabir.02091999@gmail.com>
2021-04-21 19:40:03 -0400

    usuarios

--- src/componetes/administrador/VisComAdministrador/UsuariosAd.jsx
+++ src/componetes/administrador/VisComAdministrador/UsuariosAd.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
+//materi ui
+import {TableContainer,
+        Table,
+        TableHead,
+        TableRow,
+        TableCell,
+        TableBody} from '@material-ui/core'
+
 const UsuariosAd = () => {
-    return ( <h1>usuarios</h1> );
+    return ( <div>
+        
+        <h1>usuarios</h1>
+        <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>id</TableCell>
+                            <TableCell>nombre</TableCell>
+                            <TableCell>apellido</TableCell>
+                            <TableCell>usuario</TableCell>
+                            <TableCell>mail</TableCell>
+                            <TableCell>genero</TableCell>
+                            <TableCell>clave</TableCell>
+                            <TableCell>tipo</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            {/* <TableCell>{props.usuario.nombre}</TableCell>
+                            <TableCell>{props.usuario.apellido}</TableCell>
+                            <TableCell>{props.usuario.usuario}</TableCell>
+                            <TableCell>{props.usuario.mail}</TableCell>
+                            <TableCell>{props.usuario.genero}</TableCell>
+                            <TableCell>{props.usuario.clave}</TableCell>
+                            <TableCell>{props.usuario.tipo}</TableCell> */}
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>    
+    
+        </div> );
 }
  
 export default UsuariosAd;
